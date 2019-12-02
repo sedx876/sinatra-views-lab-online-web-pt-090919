a3fb6ffdd020bdb2093b8d2e12590d4f720489aa
Sharon Watkins <swatkins876@gmail.com>
2019-12-02 14:01:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,15 @@ class App < Sinatra::Base
 		erb :index
 	end
 
-get '/hello' do
-	erb :hello
-end
+	get '/hello' do
+	    erb :hello
+	  end
+
+	  get '/goodbye' do
+	    erb :goodbye
+	  end
+
+	  get '/date' do
+	    erb :date
+	  end
 end
